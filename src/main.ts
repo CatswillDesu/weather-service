b5629ced6dd5773fe9e7053e010e87682bd81886
Max Kuznetsov <maskim.k@megasoftware.io>
2026-02-03 08:05:31 +0100

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+  });
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
